6f601a4e1ac6f4f8e146164b04c416e8af71d094
Yves Bode <ynrbode@gmail.com>
2021-09-06 23:01:07 -0300

    initial commit

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/modules/**/*.ts',
+    '!<rootDir>/src/modules/**/schemas/*.ts',
+    '!<rootDir>/src/modules/**/routes/*.ts',
+    '!<rootDir>/src/**/i-*.ts',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  // setupFiles: ['<rootDir>/jest-setup.js'],
+  clearMocks: true,
+  coverageProvider: 'v8',
+  coverageReporters: ['text-summary', 'lcov'],
+};
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const x = 1;
+const y = 2;
+
+console.log(x + y);
