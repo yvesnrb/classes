d91dc0a92e994b9975f93708a2f6f66d9abce0ca
Yves Bode <ynrbode@gmail.com>
2021-09-07 22:00:19 -0300

    feat: implements the celebrate error handler

--- src/modules/shared/http/server.ts
+++ src/modules/shared/http/server.ts
@@ -1,6 +1,7 @@
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import handleError from '@http/middleware/handle-error-middleware';
 import routes from '@http/routes';
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use(errors());
 app.use(handleError);
 
 export default app;
