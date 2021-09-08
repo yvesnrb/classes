9c8fce6b5482709f05130c54bd3db28a612d5f6a
Yves Bode <ynrbode@gmail.com>
2021-09-07 22:01:01 -0300

    feat(users): implements the auth user request schema

---
+++ src/modules/users/http/schemas/auth-user-request-schema.ts
@@ -0,0 +1,6 @@
+import { Joi } from 'celebrate';
+
+export default Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/).required(),
+});
