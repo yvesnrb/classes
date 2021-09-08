822145f2155e8f1b902c171d219a22652956a8d9
Yves Bode <ynrbode@gmail.com>
2021-09-07 22:01:48 -0300

    feat(users): implements validation in the POST / users route

--- src/modules/users/http/routes/users-route.ts
+++ src/modules/users/http/routes/users-route.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
+import { celebrate, Segments } from 'celebrate';
 
 import SessionController from '@http/controllers/session-controller';
+import authUserRequestSchema from '@http/schemas/auth-user-request-schema';
 
 const usersRouter = Router();
 const sessionController = new SessionController();
 
-usersRouter.post('/', sessionController.create.bind(sessionController));
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: authUserRequestSchema,
+  }),
+  sessionController.create.bind(sessionController),
+);
 
 export default usersRouter;
