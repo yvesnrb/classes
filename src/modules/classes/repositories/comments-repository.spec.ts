d38d3c43c5149434c4e949f24d4191bbf9266b36
Yves Bode <ynrbode@gmail.com>
2021-09-07 23:38:16 -0300

    feat(classes): implements the comments repository

---
+++ src/modules/classes/repositories/comments-repository.spec.ts
@@ -0,0 +1,112 @@
+import CommentsRepository from '@repositories/comments-repository';
+import Comment from '@entities/comment-entity';
+import mongoClient from '@/db';
+
+let commentsRepository: CommentsRepository;
+
+describe('UsersRepository', () => {
+  beforeEach(async () => {
+    commentsRepository = new CommentsRepository();
+  });
+
+  afterEach(async () => {
+    await commentsRepository.collection.drop();
+  });
+
+  beforeAll(async () => {
+    await mongoClient.connect();
+  });
+
+  afterAll(async () => {
+    await mongoClient.close();
+  });
+
+  it('should find one comment in the database', async () => {
+    await commentsRepository.collection.insertOne({
+      _id: 'mock guid',
+      id_class: 'mock class id',
+      comment: 'this is a mock comment',
+      date_created: new Date('01/01/2021 14:00:00.00Z'),
+    });
+
+    const comment = await commentsRepository.find({ _id: 'mock guid' });
+
+    expect(comment).toMatchObject({
+      _id: 'mock guid',
+      id_class: 'mock class id',
+      comment: 'this is a mock comment',
+      date_created: new Date('01/01/2021 14:00:00.00Z'),
+    });
+  });
+
+  it('should list many comments in the database', async () => {
+    const comments = Array.from({ length: 50 }, (_, index) => ({
+      _id: `${index + 1}`,
+      id_class: 'mock class id',
+      comment: 'this is a mock comment',
+      date_created: new Date('01/01/2021 14:00:00.00Z'),
+    }));
+
+    const partialComments = Array.from({ length: 10 }, (_, index) => ({
+      _id: `${index + 1}`,
+      id_class: 'mock class id',
+      comment: 'this is a mock comment',
+    }));
+
+    await commentsRepository.collection.insertMany(comments);
+
+    const list = await commentsRepository.list(
+      { id_class: 'mock class id' },
+      0,
+      10,
+    );
+
+    expect(list).toEqual(
+      expect.arrayContaining(
+        partialComments.map((comment) => expect.objectContaining(comment)),
+      ),
+    );
+  });
+
+  it('should save a comment in the database', async () => {
+    const comment = new Comment('mock class id', 'this is a mock comment');
+
+    await commentsRepository.save(comment);
+
+    const insertedComment = await commentsRepository.collection.findOne({
+      id_class: 'mock class id',
+    });
+
+    expect(insertedComment).toMatchObject({
+      id_class: 'mock class id',
+      comment: 'this is a mock comment',
+    });
+  });
+
+  it('should update a saved comment', async () => {
+    await commentsRepository.collection.insertOne({
+      _id: 'mock guid',
+      id_class: 'mock class id',
+      comment: 'this is a mock comment',
+      date_created: new Date('01/01/2021 14:00:00.00Z'),
+    });
+
+    await commentsRepository.update({
+      _id: 'mock guid',
+      id_class: 'mock class id',
+      comment: 'this is an updated comment',
+      date_created: new Date('01/01/2021 14:00:00.00Z'),
+    });
+
+    const updatedComment = await commentsRepository.collection.findOne({
+      _id: 'mock guid',
+    });
+
+    expect(updatedComment).toMatchObject({
+      _id: 'mock guid',
+      id_class: 'mock class id',
+      comment: 'this is an updated comment',
+      date_created: new Date('01/01/2021 14:00:00.00Z'),
+    });
+  });
+});
---
+++ src/modules/classes/repositories/comments-repository.ts
@@ -0,0 +1,81 @@
+import { Collection, Filter } from 'mongodb';
+
+import Comment from '@entities/comment-entity';
+import mongoConfig from '@config/mongodb';
+import mongoClient from '@/db';
+
+export default class CommentsRepository {
+  public collection: Collection<Comment>;
+
+  constructor() {
+    this.collection = mongoClient
+      .db(mongoConfig.database)
+      .collection<Comment>('comments');
+  }
+
+  /**
+   * Find one comment in the database.
+   *
+   * @param filters - MongoDB filters for the Comment entity.
+   *
+   * @returns The comment if found, null otherwise.
+   */
+  public async find(filters: Filter<Comment>): Promise<Comment | null> {
+    await mongoClient.connect();
+    const comment = await this.collection.findOne(filters);
+
+    return comment;
+  }
+
+  /**
+   * Lists many comments from the database.
+   *
+   * @param filters - MongoDB filters for the Comment entity.
+   *
+   * @param skip - Skip this many documents in the list.
+   *
+   * @param limit - Limit the list size to this many documents.
+   *
+   * @returns An array of comments.
+   */
+  public async list(
+    filters: Filter<Comment>,
+    skip: number,
+    limit: number,
+  ): Promise<Comment[]> {
+    await mongoClient.connect();
+    const list = this.collection
+      .find(filters)
+      .limit(limit)
+      .skip(skip)
+      .toArray();
+
+    return list;
+  }
+
+  /**
+   * Save a new comment entity to the database.
+   *
+   * @param comment - The comment entity to save.
+   */
+  public async save(comment: Comment): Promise<void> {
+    await mongoClient.connect();
+    await this.collection.insertOne(comment);
+  }
+
+  /**
+   * Update an existing comment in the database. Ensure that the _id field of
+   * the entity matches the one you are trying to update.
+   *
+   * @param comment - The comment entity to update.
+   */
+  public async update(comment: Comment): Promise<void> {
+    await mongoClient.connect();
+    await this.collection.updateOne(
+      { _id: comment._id },
+      {
+        $set: { ...comment, date_updated: new Date() },
+      },
+    );
+  }
+}
