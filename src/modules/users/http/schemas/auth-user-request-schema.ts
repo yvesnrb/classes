57ad9d6f2927b5b16e51d3117a3003294b5c897a
Yves Bode <ynrbode@gmail.com>
2021-09-09 23:26:49 -0300

    refactor(users): refactors auth-user-request-schema
    The file should now respect the 80 column limit.

--- src/modules/users/http/schemas/auth-user-request-schema.ts
+++ src/modules/users/http/schemas/auth-user-request-schema.ts
@@ -2,5 +2,7 @@ import { Joi } from 'celebrate';
 
 export default Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/).required(),
+  password: Joi.string()
+    .pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+    .required(),
 });
