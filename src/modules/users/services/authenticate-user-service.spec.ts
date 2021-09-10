e9e143b4a9f429eab756354e3c0d3ed8cfa49fb9
Yves Bode <ynrbode@gmail.com>
2021-09-10 11:16:44 -0300

    refactor(users): refactors the authenticate user service to follow the camel case rule

--- src/modules/users/services/authenticate-user-service.spec.ts
+++ src/modules/users/services/authenticate-user-service.spec.ts
@@ -2,70 +2,72 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 import AuthenticateUserService from '@services/authenticate-user-service';
+import UsersRepository from '@repositories/users-repository';
+import User from '@entities/user-entity';
 import AppError from '@errors/app-error';
 
-const mockFind = jest.fn().mockImplementation(() => ({
-  _id: 'mock id',
+let authenticateUser: AuthenticateUserService;
+
+const mockUser: User = {
+  _id: 'mock-user-id',
   name: 'John Doe',
   email: 'j.doe@mail.com',
-  password: 'mock hash',
-  date_created: new Date('2021-01-01 14:00:00'),
-  date_updated: new Date('2021-01-01 14:00:00'),
+  password: 'mock-hash',
+  dateCreated: new Date('2021-01-01 14:00:00'),
+  dateUpdated: new Date('2021-01-01 14:00:00'),
+};
+
+jest.mock('@config/jwt', () => ({
+  secret: 'unsafe secret',
+  expiration: '1h',
 }));
 
-jest.mock('@repositories/users-repository', () => {
-  return jest.fn().mockImplementation(() => {
-    return {
-      find: mockFind,
-    };
-  });
-});
-
-jest.mock('bcryptjs', () => {
-  return {
-    compareSync: jest.fn().mockImplementation(() => true),
-  };
-});
-
-jest.mock('jsonwebtoken', () => {
-  return {
-    sign: jest.fn().mockImplementation(() => 'mock token'),
-  };
-});
-
-jest.mock('@config/jwt', () => {
-  return {
-    secret: 'unsafe secret',
-    expiration: '1h',
-  };
-});
-
 describe('AuthenticateUserService', () => {
+  beforeEach(() => {
+    authenticateUser = new AuthenticateUserService();
+  });
+
   it('should auth an user with the correct credentials', async () => {
-    const authenticateUser = new AuthenticateUserService();
+    const usersRepositoryFindSpy = jest
+      .spyOn(UsersRepository.prototype, 'find')
+      .mockReturnValue(Promise.resolve(mockUser));
+
+    const bcryptCompareSyncSpy = jest
+      .spyOn(bcrypt, 'compareSync')
+      .mockReturnValue(true);
+
+    const jwtSignSpy = jest
+      .spyOn(jwt, 'sign')
+      .mockReturnValue('mock-token' as unknown as any);
 
     const credentials = await authenticateUser.execute({
       email: 'j.doe@mail.com',
       password: 'pass',
     });
 
-    expect(mockFind).toHaveBeenCalledWith({ email: 'j.doe@mail.com' });
-    expect(bcrypt.compareSync).toHaveBeenCalledWith('pass', 'mock hash');
-    expect(jwt.sign).toHaveBeenCalledWith({}, 'unsafe secret', {
-      subject: 'mock id',
+    expect(usersRepositoryFindSpy).toHaveBeenCalledWith({
+      email: 'j.doe@mail.com',
+    });
+    expect(bcryptCompareSyncSpy).toHaveBeenCalledWith(
+      'pass',
+      'mock-hash',
+    );
+    expect(jwtSignSpy).toHaveBeenCalledWith({}, 'unsafe secret', {
+      subject: 'mock-user-id',
       expiresIn: '1h',
     });
     expect(credentials).toMatchObject({
-      _id: 'mock id',
+      _id: 'mock-user-id',
       name: 'John Doe',
       email: 'j.doe@mail.com',
-      token: 'mock token',
+      token: 'mock-token',
     });
   });
 
   it('should not authenticate an user that is not registered', async () => {
-    const authenticateUser = new AuthenticateUserService();
-    mockFind.mockImplementationOnce(() => null);
+    const usersRepositoryFindSpy = jest
+      .spyOn(UsersRepository.prototype, 'find')
+      .mockReturnValue(Promise.resolve(null));
 
     await expect(
       authenticateUser.execute({
@@ -73,11 +75,19 @@ describe('AuthenticateUserService', () => {
         password: 'pass',
       }),
     ).rejects.toThrowError(new AppError('no email auth attempt', 403));
+    expect(usersRepositoryFindSpy).toHaveBeenCalledWith({
+      email: 'j.doe@mail.com',
+    });
   });
 
   it('should not authenticate an user with the wrong password', async () => {
-    const authenticateUser = new AuthenticateUserService();
-    jest.spyOn(bcrypt, 'compareSync').mockImplementationOnce(() => false);
+    const usersRepositoryFindSpy = jest
+      .spyOn(UsersRepository.prototype, 'find')
+      .mockReturnValue(Promise.resolve(mockUser));
+
+    const bcryptCompareSyncSpy = jest
+      .spyOn(bcrypt, 'compareSync')
+      .mockReturnValue(false);
 
     await expect(
       authenticateUser.execute({
@@ -85,5 +95,9 @@ describe('AuthenticateUserService', () => {
         password: 'pass',
       }),
     ).rejects.toThrowError(new AppError('wrong password auth attempt', 403));
+    expect(usersRepositoryFindSpy).toHaveBeenCalledWith({
+      email: 'j.doe@mail.com',
+    });
+    expect(bcryptCompareSyncSpy).toHaveBeenCalledWith('pass', 'mock-hash');
   });
 });
